1eb32ede07bd08011515f702c3ec9a884ca58be2
Richard Yang <rich.yang88@gmail.com>
2019-08-22 14:11:12 -0400

    added views and models function

--- models/connection.js
+++ models/connection.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
  * This will need to change for every new project you create.
  *
  */
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost/<db-name>";
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost/murmuring-island-31763.gi";
 
 
 /* Step 2
---
+++ models/pets.js
@@ -0,0 +1,81 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 1 alternative
+ *
+ * TODO: make a global variable to act as an in memory database. 
+ * NOTE: doing this WILL NOT persist your data and you will loose
+ * your data once you stop running your server.
+ *
+ */
+// global.sampleModel = [];
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const petSchema = new mongoose.Schema({
+ name: String,
+ type: String,
+ age: Number,
+ gender: String,
+ status: String
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const petCollection = mongoose.model('Pet', petSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+function getAllPets() {
+  return petCollection.find()
+}
+
+function getOnePet(id){
+  return petCollection.findById(id)
+}
+
+function addOnePet(newPet){
+  return petCollection.create(newPet)
+}
+
+function updatePet(id, petInfo){
+  return petCollection.findByIdAndUpdate(id, petInfo)
+}
+
+function deletePetInfoById(id){
+  return issueCollection.findByIdAndDelete(id)
+}
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  getAllPets,
+  getOnePet,
+  addOnePet,
+  updatePet,
+  deletePetInfoById
+}
