0d4eb3166d62bc58b5c7d2d124ab0e1e1d402685
Richard Yang <rich.yang88@gmail.com>
2019-08-25 21:57:24 -0400

    add pet HBS working

--- controllers/pets.js
+++ controllers/pets.js
@@ -60,8 +60,8 @@ petRouter.get('/listAll', (req, res) => {
 // })
 
 petRouter.get('/addPet', (req,res)=>{
-  petApi.addOnepet(req.params.petId).then(()=>{
-    res.render('./pets/createPetHBS', {})
+  petApi.addOnePet(req.params.petId).then(()=>{
+    res.render('./petShop/createPetHBS', {})
   })
 })
 
